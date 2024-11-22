c72e1579d6788eab361bafd52e2c346f80be9166
Mayya Ragimova <abeillee@bk.ru>
2024-11-22 14:31:53 +0300

    feat: add best_gifts_cards.js

---
+++ christmas-shop/scripts/best_gifts_cards.js
@@ -0,0 +1,4 @@
+import { CARDS_BOX } from "./constants.js";
+import { generateRandomCards } from "./cards_rendering.js";
+
+generateRandomCards(CARDS_BOX, 4);
--- christmas-shop/scripts/cards_rendering.js
+++ christmas-shop/scripts/cards_rendering.js
@@ -1,15 +1,15 @@
 import { cardsArray } from "./constants.js";
-import { CARDS_BOX } from "./constants.js";
 
-//создание пустой карточки
-const createCardTemplate = () => {
+
+//Создание пустой карточки
+export const createCardTemplate = () => {
     const card1 = document.createElement('div');
     card1.classList.add('card');
     return card1;
 }
 
-//генерация индексов
-const generateRandomIndexes = (amountOfCards) => {
+//Генерация индексов
+export const generateRandomIndexes = (amountOfCards) => {
     let number = cardsArray.length;
     const arrOfIndexes = [];
     while (arrOfIndexes.length < amountOfCards) {
@@ -21,19 +21,19 @@ const generateRandomIndexes = (amountOfCards) => {
     return arrOfIndexes;
 }
 
-//создание шаблона карточки
-const createCard = (card, i) => {
+//Создание шаблона карточки
+export const createCard = (card, i) => {
     card.innerHTML = `<img class="card__img" src="./images/gift-for-work.png" alt="gift-for-work"> 
             <div class="card__description">
-                <h4 class="title4 title4--purple title-card">${cardsArray[i].category}</h4>
+                <h4 class="title4 title-card">${cardsArray[i].category}</h4>
                 <h3 class="title3">${cardsArray[i].name}</h3>
             </div>`;
     return card;
 }
 
-//установка цвета для title
-const setColorTitle = (array, id, card) => {
-    const cardTitle = card.querySelector('.card .title-card');
+//Установка цвета для title
+export const setColorTitle = (array, id, card, containerClass) => {
+    const cardTitle = card.querySelector(`.${containerClass} .title-card`);
     if (array[id].category === 'For Work') {
         cardTitle.classList.add('title4--purple');
     } else if (array[id].category === 'For Health') {
@@ -44,19 +44,23 @@ const setColorTitle = (array, id, card) => {
 };
 
 //Установка картинки для карточки подарка
-const setImage = (container, i, classImg) => {
+export const setImage = (container, i, classImg) => {
     const cardImg = container.querySelector(`.${classImg}`); {
         if (cardsArray[i].category === 'For Work') {
             cardImg.setAttribute('src', './images/gift-for-work.png');
+            cardImg.setAttribute('alt', 'gift-for-work');
         } else if (cardsArray[i].category === 'For Health') {
             cardImg.setAttribute('src', './images/gift-for-health.png');
+            cardImg.setAttribute('alt', 'gift-for-health');
         } else if (cardsArray[i].category === 'For Harmony') {
             cardImg.setAttribute('src', './images/gift-for-harmony.png');
+            cardImg.setAttribute('alt', 'gift-for-harmony');
         }
     }
 }
 
-const generateRandomCards = (container, amountOfCards) => {
+//Генерация рандомных карточек
+export const generateRandomCards = (container, amountOfCards) => {
     container.innerHTML = '';
     let indexes = generateRandomIndexes(amountOfCards);
 
@@ -64,10 +68,10 @@ const generateRandomCards = (container, amountOfCards) => {
         let cardNew = createCardTemplate();
         cardNew.setAttribute('id', i);
         cardNew = createCard(cardNew, i);
-        setColorTitle(cardsArray, i, cardNew);
+        setColorTitle(cardsArray, i, cardNew, 'card');
         setImage(cardNew, i, 'card__img');
         console.log(cardNew);
         container.appendChild(cardNew);
     }
 }
-generateRandomCards(CARDS_BOX, 4);
+
